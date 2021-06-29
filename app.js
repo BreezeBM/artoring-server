8da4a319802606e9cc577df53dc76e1526f9ab9b
zpxlffjrm <scm0222@naver.com>
2021-06-29 20:45:08 +0900

    CD test. add health check

--- app.js
+++ app.js
@@ -7,6 +7,9 @@ const app = express();
 app.use(express.json({ extended: false }));
 app.use(cors());
 
+app.get('/', (req, res) => {
+  res.send();
+});
 module.exports = app.listen(port, () => {
   console.log(`🚀 Server is starting on ${port}`);
 });

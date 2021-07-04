53734b1d1727c8b005c083fda08d24654a429710
yrkimyy <kellyyeoraekim@gmail.com>
2021-07-04 13:41:16 +0900

    test for / route

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
 const express = require('express');
 const cors = require('cors');
-const port = 3000;
+const port = 8080;
 
 const app = express();
 
 app.use(express.json({ extended: false }));
 app.use(cors());
+
+
+app.get("/", (req, res) => {
+  res.send("Hello World")
+})
+
 
 module.exports = app.listen(port, () => {
   console.log(`🚀 Server is starting on ${port}`);

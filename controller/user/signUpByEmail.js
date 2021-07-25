6e4de68f6684305c81820f7c8d73379f618d3f7d
yrkimyy <kellyyeoraekim@gmail.com>
2021-07-25 14:57:04 +0900

    회원가입시 이메일 확인

--- controller/user/signUpByEmail.js
+++ controller/user/signUpByEmail.js
@@ -1,3 +1,18 @@
+const { userModel } = require('../../model');
+const { verifyJWTToken } = require('../tools');
+
 module.exports = async (req, res) => {    
-    console.log("YEAY")
+
+    const {email, name, gender, birth, mobile, address, password } = req.body
+
+
+
+    if(email) {
+        const data = await userModel.find({ email: email })
+        if(data){
+            res.status(200).send({message : "이미 가입된 이메일입니다."})
+        }
+    }
+
+
 }

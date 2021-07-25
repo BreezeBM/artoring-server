d563e63a441397780d9e408adf6cdc9f1804c724
zpxlffjrm <scm0222@naver.com>
2021-07-25 21:07:27 +0900

    start CD progress16

--- scripts/stop_server.sh
+++ scripts/stop_server.sh
@@ -1,7 +1,7 @@
 if test -f /home/ec2-user/artoring-server/app.js;then
   cd /home/ec2-user/artoring-server
 
-  if test -f /home/ec2-user/artoring-server/node_modules
+  if test ! -f /home/ec2-user/artoring-server/node_modules
     npm install
   fi
   npm run stop

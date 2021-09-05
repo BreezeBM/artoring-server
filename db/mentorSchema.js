3668ffa916bf79ffdef46014c24d58739942ad5c
zpxlffjrm <scm0222@naver.com>
2021-09-05 13:01:31 +0900

    fix bugs from QA and update mentor schema & get filtered cardlist as mentor category

--- db/mentorSchema.js
+++ db/mentorSchema.js
@@ -8,6 +8,25 @@ const mentorSchema = new Schema({
   thumb: { type: String, required: true }, // 어드민 관리용
   settledAmount: Number,
   tags: [String],
+  // 멘토링 가능한 영역, 연차정보
+  category: {
+    // 값이 5 === 해당사항 없음 1 === 모든 년차 멘토링 가능
+
+    // 교육
+    employment: { type: Number, default: 5 },
+
+    // 창업
+    founded: { type: Number, default: 5 },
+
+    // 전문교육
+    professional: { type: Number, default: 5 },
+
+    // 프리랜서
+    free: { type: Number, default: 5 },
+
+    // 진학
+    edu: { type: Number, default: 5 }
+  },
   descriptionForMentor: { type: String, required: true },
   descriptionText: { type: String, required: true }, // 한글만 존재하는 텍스트. 역시 엘라스틱 서치 검색용
   availableTime: { mon: [String], tue: [String], wed: [String], thu: [String], fri: [String], sat: [String], sun: [String] },

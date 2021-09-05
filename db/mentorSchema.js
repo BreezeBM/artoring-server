bac3627b0fcd38b998981e926abb1ca84668ac82
zpxlffjrm <scm0222@naver.com>
2021-09-05 13:06:24 +0900

    update schema and comment

--- db/mentorSchema.js
+++ db/mentorSchema.js
@@ -10,22 +10,22 @@ const mentorSchema = new Schema({
   tags: [String],
   // 멘토링 가능한 영역, 연차정보
   category: {
-    // 값이 5 === 해당사항 없음 1 === 모든 년차 멘토링 가능
+    // 값이 -1 === 해당사항 없음 4 === 모든 년차 멘토링 가능 이하 내림차순
 
     // 교육
-    employment: { type: Number, default: 5 },
+    employment: { type: Number, default: -1 },
 
     // 창업
-    founded: { type: Number, default: 5 },
+    founded: { type: Number, default: -1 },
 
     // 전문교육
-    professional: { type: Number, default: 5 },
+    professional: { type: Number, default: -1 },
 
     // 프리랜서
-    free: { type: Number, default: 5 },
+    free: { type: Number, default: -1 },
 
     // 진학
-    edu: { type: Number, default: 5 }
+    edu: { type: Number, default: -1 }
   },
   descriptionForMentor: { type: String, required: true },
   descriptionText: { type: String, required: true }, // 한글만 존재하는 텍스트. 역시 엘라스틱 서치 검색용

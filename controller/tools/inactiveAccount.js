2b445a6fa174916abf166d318a6f4abb149fdf18
BreezeBM <seyya77@gmail.com>
2021-09-07 16:21:48 +0900

    add user active condition change 70%

---
+++ controller/tools/inactiveAccount.js
@@ -0,0 +1,45 @@
+const schedule = require("node-schedule");
+const { userModel } = require("../../model");
+// const { startSession } = require("mongoose");
+
+// 매일 10시에 User Table을 조회한다.
+const inactiveAccount = schedule.scheduleJob(
+  "00 00 10 * * 0-6",
+  function async() {
+    // transaction
+    // const session = await MyModel.startSession();
+    try {
+      let lastYear = new Date();
+      lastYear.setTime(lastYear.getTime() - (365 * 24 * 60 * 60 * 1000));
+
+      // session.startTransaction();
+
+      // User Table에서, active컬럼이 true인 사람중에, 마지막 접속이 현재 시간 기준으로 1년이 넘은 유저를 찾는다.
+      const targets = await userModel.find({
+        active: true,
+        loginedAt: { $lte: lastYear },
+      }).sort({ loginedAt: 1 });
+      // 위의 조건에 맞는 모든 유저에게 휴면계정 이메일 전송 및 active컬럼 false로 변경
+      targets.forEach((target) => {
+        const userData = await userModel.find({ _id: target._id }).select({
+          _id: 0,
+          email: 1,
+        });
+      });
+      // 이메일 전송
+
+      // active 변경
+      await userModel.updateOne({ _id: target._id }, {
+        $set: { active: false },
+      });
+      // await session.commitTransaction();
+      // session.endSession();
+    } catch (error) {
+      // await session.abortTransaction();
+      // session.endSession();
+      console.error(error);
+    }
+  },
+);
+
+module.exports = inactiveAccount;
--- db/userSchema.js
+++ db/userSchema.js
@@ -1,11 +1,12 @@
-
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-
-  thumb: { type: String, default: 'https://artoring.com/image/1626851218536.png' },
+  thumb: {
+    type: String,
+    default: "https://artoring.com/image/1626851218536.png",
+  },
   name: { type: String },
   appId: String, // 소셜로그인시 탈퇴를 위해 appId를 저장해둠
   snsType: String,
@@ -23,11 +24,11 @@ const userSchema = new Schema({
     jobTitle: { type: String },
     belongs: { type: String },
     howLong: { type: String },
-    dept: String
+    dept: String,
   },
   interestedIn: [{
     name: String,
-    val: Boolean
+    val: Boolean,
   }],
 
   likedCareerEdu: [Schema.ObjectId],
@@ -36,7 +37,9 @@ const userSchema = new Schema({
   outdoorAct: String,
   workHistory: String,
   createdAt: { type: Date, default: new Date() },
-  refOrLongTimeToken: String
+  refOrLongTimeToken: String,
+  active: { type: Boolean, default: true },
+  loginedAt: Date,
 });
 
 module.exports = userSchema;

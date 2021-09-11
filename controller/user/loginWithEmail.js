dc93f3b2b6f378112cc72c9a56fdacad313077a3
BreezeBM <seyya77@gmail.com>
2021-09-11 17:11:35 +0900

    feat timestamp for login

--- controller/user/loginWithEmail.js
+++ controller/user/loginWithEmail.js
@@ -1,18 +1,19 @@
-require('dotenv').config();
-const { userModel } = require('../../model');
-const { createJWT, sha256Encrypt } = require('../tools');
-const bcrypt = require('bcrypt');
+require("dotenv").config();
+const { userModel } = require("../../model");
+const { createJWT, sha256Encrypt } = require("../tools");
+const bcrypt = require("bcrypt");
 
 module.exports = async (req, res) => {
   const { password, email } = req.body;
-  const hashingTime = process.env.NODE_ENV === 'development'
+  const hashingTime = process.env.NODE_ENV === "development"
     ? process.env.HASHING_TIME_DEV
     : process.env.HASHING_TIME_PRO;
-  const salt = process.env.NODE_ENV === 'development'
+  const salt = process.env.NODE_ENV === "development"
     ? process.env.SALT_DEV
     : process.env.SALT_PRO;
 
   if (email) {
+    await userModel.updateOne({ email }, { $set: { loginedAt: new Date() } });
     userModel.findOne({ email })
       .select({
         _id: 1,
@@ -26,29 +27,32 @@ module.exports = async (req, res) => {
         likedMentor: 1,
         likedInfo: 1,
         verifiedEmail: 1,
-        createdAt: 1
+        createdAt: 1,
       })
       .then((data) => {
         if (data) {
           bcrypt.compare(password, data.pwd)
-            .then(async result => {
+            .then(async (result) => {
               if (result) {
                 delete data.pwd;
-                const token = await createJWT({ _id: data._id, name: data.name }, 3600);
+                const token = await createJWT({
+                  _id: data._id,
+                  name: data.name,
+                }, 3600);
                 res.status(201).json({ accessToken: token, userData: data });
               } else {
-                res.status(401).send({ message: '잘못된 비밀번호' });
+                res.status(401).send({ message: "잘못된 비밀번호" });
               }
             });
         } else {
-          res.status(404).send({ message: '유저정보를 찾을수 없습니다.' });
+          res.status(404).send({ message: "유저정보를 찾을수 없습니다." });
         }
       })
-      .catch(e => {
+      .catch((e) => {
         console.log(e);
         res.status(500).json(e.message);
       });
   } else {
-    res.status(400).send({ message: 'Invalid Access' });
+    res.status(400).send({ message: "Invalid Access" });
   }
 };

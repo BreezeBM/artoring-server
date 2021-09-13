eba364934e2a5a9f065f6ae2adbb52290081695e
zpxlffjrm <scm0222@naver.com>
2021-09-13 11:38:36 +0900

    update admin get purchase list, pagination in purchaselist, purchase schema and integrate iamport

--- controller/user/postPerchase.js
+++ controller/user/postPerchase.js
@@ -1,5 +1,5 @@
 const { userModel, purchaseHistoryModel, careerInfoModel, mentoringModel, mongoose } = require('../../model');
-const { verifyJWTToken, verifyAndCallback } = require('../tools');
+const { verifyJWTToken, verifyAndCallback, sha256Encrypt } = require('../tools');
 
 module.exports = async (req, res) => {
   const { loginType: type, cardId, userId, reservationType, startDate, endDate } = req.body;
@@ -32,6 +32,7 @@ module.exports = async (req, res) => {
               readConcern: { level: 'majority' },
               writeConcern: { w: 'majority' }
             };
+            let createdDoc;
             // 사실 몽고디비는 하나의 도큐먼트에 대해 atomic 하다...
             // 혹시몰라 우선은 transaction을 사용함.
             await session.withTransaction(() => {
@@ -42,23 +43,33 @@ module.exports = async (req, res) => {
                   price: targetData.price,
                   bookedStartTime: startDate || targetData.startDate,
                   bookedEndTime: endDate || targetData.endDate,
+                  // 아임포트에서 관리하는 merchant_id 는 40글자가 최대이다.
+                  merchantUid: `mid_${sha256Encrypt(36, _id, Date().toString())}`,
                   originType: reservationType,
                   loginType: type,
                   startDate,
                   endDate
                 })
-                .then(() => {
+                .then((data) => {
+                  createdDoc = data;
                   return mentoringModel.findById({ _id: mongoose.Types.ObjectId(cardId) });
                 })
+                .then((data) => {
+                  createdDoc.programTitle = data.title;
+                  return userModel.findById({ _id: mongoose.Types.ObjectId(_id) });
+                })
                 .then(data => {
+                  // 예약이 완료되지 않았더라도 예약자 추가시켜 추가 예약을 방지
                   if (data.joinedParticipants >= data.maximumParticipants) res.status(400).send();
                   else {
+                    createdDoc.userName = data.name;
+                    createdDoc.userPhone = data.phone;
                     return mentoringModel.findByIdAndUpdate({ _id: mongoose.Types.ObjectId(cardId) }, { $inc: { joinedParticipants: 1 } });
                   }
                 });
             }, transactionOptions);
             session.endSession();
-            res.status(201).send();
+            res.status(201).send(createdDoc);
           }
         }
       }
@@ -73,6 +84,7 @@ module.exports = async (req, res) => {
           readConcern: { level: 'majority' },
           writeConcern: { w: 'majority' }
         };
+        const createdDoc = {};
         await session.withTransaction(() => {
           return purchaseHistoryModel.create(
             {
@@ -81,23 +93,32 @@ module.exports = async (req, res) => {
               price: targetData.price,
               bookedStartTime: startDate || targetData.startDate,
               bookedEndTime: endDate || targetData.endDate,
+              merchantUid: `mid_${sha256Encrypt(36, userId, Date().toString())}`,
               originType: reservationType,
               loginType: type,
               startDate,
               endDate
             })
-            .then(() => {
+            .then((data) => {
+              for (const n of Object.keys(data._doc)) { createdDoc[n] = data._doc[n]; }
               return mentoringModel.findById({ _id: mongoose.Types.ObjectId(cardId) });
             })
+            .then((data) => {
+              createdDoc.programTitle = data._doc.title;
+              return userModel.findById({ _id: mongoose.Types.ObjectId(userId) });
+            })
             .then(data => {
+              // 예약이 완료되지 않았더라도 예약자 추가시켜 추가 예약을 방지
               if (data.joinedParticipants >= data.maximumParticipants) res.status(400).send();
               else {
+                createdDoc.userName = data._doc.name;
+                createdDoc.userPhone = data._doc.phone;
                 return mentoringModel.findByIdAndUpdate({ _id: mongoose.Types.ObjectId(cardId) }, { $inc: { joinedParticipants: 1 } });
               }
             });
         }, transactionOptions);
         session.endSession();
-        res.status(201).send();
+        res.status(201).send(createdDoc);
       }, type, accessToken, res);
     }
   } catch (e) {

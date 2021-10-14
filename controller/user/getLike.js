6a75dd8a064e7660f1f936db5f6e8ed287900d15
zpxlffjrm <scm0222@naver.com>
2021-10-15 08:04:28 +0900

    fix token check in likes handler;

--- controller/user/getLike.js
+++ controller/user/getLike.js
@@ -4,6 +4,10 @@ const { verifyJWTToken, verifyAndCallback } = require('../tools');
 module.exports = async (req, res) => {
   const decode = await verifyJWTToken(req);
 
+  if (!req.cookies.authorization) {
+    res.status(401).send();
+    return;
+  }
   const split = req.cookies.authorization.split(' ');
   const accessToken = split[0].concat(' ', split[1]);
   const type = split[2];

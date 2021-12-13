a59c9370488e0ff0f4be7213fffd6756f8e4b247
zpxlffjrm <scm0222@naver.com>
2021-12-13 14:27:20 +0900

    update init bash script

--- .platform/hooks/prebuild/init.sh
+++ .platform/hooks/prebuild/init.sh
@@ -11,6 +11,7 @@ if [[ ! -d "/home/webapp/dl.fedoraproject.org" ]]; then
   sudo systemctl start fcgiwrap@nginx.socket;
   sudo systemctl enable fcgiwrap@nginx.socket;
   sudo yum -y install conntrack-tools;
+  sudo yum -y install firewalld;
 
   # letsEncrypt
   sudo yum install -y certbot python2-certbot-nginx;
@@ -19,71 +20,86 @@ if [[ ! -d "/home/webapp/dl.fedoraproject.org" ]]; then
   sudo ln -sf /etc/letsencrypt/live/${DOMAIN} /etc/letsencrypt/ebcert;
 
   # honeypot 파일
-  sudo touch /etc/nginx/includes/honeypot.conf;
-  sudo echo $"fastcgi_intercept_errors off;" >> /etc/nginx/includes/honeypot.conf;
-  sudo echo $"include /etc/nginx/fastcgi_params;" >> /etc/nginx/includes/honeypot.conf;
-  sudo echo $"fastcgi_param SCRIPT_FILENAME /usr/local/libexec/block-ip.cgi;" >> /etc/nginx/includes/honeypot.conf;
-  sudo echo $"fastcgi_pass unix:/run/fcgiwrap/fcgiwrap-nginx.sock;" >> /etc/nginx/includes/honeypot.conf;
+  if [[ ! -d "/etc/nginx/includes" ]]; then
+    sudo mkdir /etc/nginx/includes
+  fi
+  if [[ ! -f "/etc/nginx/includes/honeypot.conf" ]]; then
+    sudo touch /etc/nginx/includes/honeypot.conf;
+    sudo echo $"fastcgi_intercept_errors off;" >> /etc/nginx/includes/honeypot.conf;
+    sudo echo $"include /etc/nginx/fastcgi_params;" >> /etc/nginx/includes/honeypot.conf;
+    sudo echo $"fastcgi_param SCRIPT_FILENAME /usr/local/libexec/block-ip.cgi;" >> /etc/nginx/includes/honeypot.conf;
+    sudo echo $"fastcgi_pass unix:/run/fcgiwrap/fcgiwrap-nginx.sock;" >> /etc/nginx/includes/honeypot.conf;
+  fi
 
   # cgi 파일
-  sudo touch /usr/local/libexec/block-ip.cgi;
-  sudo echo $"#!/bin/bash" >> /usr/local/libexec/block-ip.cgi;
-  sudo echo $"" >> /usr/local/libexec/block-ip.cgi;
-  sudo echo $"echo \"Status: 410 Gone\"" >> /usr/local/libexec/block-ip.cgi;
-  sudo echo $"echo \"Content-type: text/plain\"" >> /usr/local/libexec/block-ip.cgi;
-  sudo echo $"echo" >> /usr/local/libexec/block-ip.cgi;
-  sudo echo $"" >> /usr/local/libexec/block-ip.cgi;
-  sudo echo $"echo \"Bye bye, \$REMOTE_ADDR!\"" >> /usr/local/libexec/block-ip.cgi;
-  sudo echo $"sudo /usr/local/bin/block-ip.sh" >> /usr/local/libexec/block-ip.cgi;
-  sudo echo $"" >> /usr/local/libexec/block-ip.cgi;
-  sudo echo $"exit 0" >> /usr/local/libexec/block-ip.cgi;
+  if [[ ! -f "/usr/local/libexec/block-ip.cgi" ]]; then
+    sudo touch /usr/local/libexec/block-ip.cgi;
+    sudo echo $"#!/bin/bash" >> /usr/local/libexec/block-ip.cgi;
+    sudo echo $"" >> /usr/local/libexec/block-ip.cgi;
+    sudo echo $"echo \"Status: 410 Gone\"" >> /usr/local/libexec/block-ip.cgi;
+    sudo echo $"echo \"Content-type: text/plain\"" >> /usr/local/libexec/block-ip.cgi;
+    sudo echo $"echo" >> /usr/local/libexec/block-ip.cgi;
+    sudo echo $"" >> /usr/local/libexec/block-ip.cgi;
+    sudo echo $"echo \"Bye bye, \$REMOTE_ADDR!\"" >> /usr/local/libexec/block-ip.cgi;
+    sudo echo $"sudo /usr/local/bin/block-ip.sh" >> /usr/local/libexec/block-ip.cgi;
+    sudo echo $"" >> /usr/local/libexec/block-ip.cgi;
+    sudo echo $"exit 0" >> /usr/local/libexec/block-ip.cgi;
 
-  sudo chmod 0755 /usr/local/libexec/block-ip.cgi;
+    sudo chmod 0755 /usr/local/libexec/block-ip.cgi;
+  fi
 
   # sh 파일
-  sudo touch /usr/local/bin/block-ip.sh;
-  sudo echo $"#!/bin/bash" >> /usr/local/bin/block-ip.sh;
-  sudo echo $"" >> /usr/local/bin/block-ip.sh;
-  sudo echo $"if [[ -z \${REMOTE_ADDR} ]]; then" >> /usr/local/bin/block-ip.sh;
-  sudo echo $"  if [[ -z \"\$1\"  ]]; then" >> /usr/local/bin/block-ip.sh;
-  sudo echo $"    echo \"REMOTE_ADDR not set!\"" >> /usr/local/bin/block-ip.sh;
-  sudo echo $"    exit 1" >> /usr/local/bin/block-ip.sh;
-  sudo echo $"  else" >> /usr/local/bin/block-ip.sh;
-  sudo echo $"    REMOTE_ADDR=\$1" >> /usr/local/bin/block-ip.sh;
-  sudo echo $"  fi" >> /usr/local/bin/block-ip.sh;
-  sudo echo $"fi" >> /usr/local/bin/block-ip.sh;
-  sudo echo $"" >> /usr/local/bin/block-ip.sh;
-  sudo echo $"# Put space separate list of trusted IP addresses, not to lock yourself out if you like to test the honeypot! \:\)" >> /usr/local/bin/block-ip.sh;
-  sudo echo $"TRUSTED_IPS=(127.0.0.1)" >> /usr/local/bin/block-ip.sh;
-  sudo echo $"if printf '%s\n' \${TRUSTED_IPS[@]} | grep -q -P \"^\$REMOTE_ADDR\\\$\"; then" >> /usr/local/bin/block-ip.sh;
-  sudo echo $"  echo \"Trusted IP\"" >> /usr/local/bin/block-ip.sh;
-  sudo echo $"  exit 0" >> /usr/local/bin/block-ip.sh;
-  sudo echo $"fi" >> /usr/local/bin/block-ip.sh;
-  sudo echo $"" >> /usr/local/bin/block-ip.sh;
-  sudo echo $"if [[ \"\$REMOTE_ADDR\" != \"\${1#*[0-9].[0-9]}\" ]]; then" >> /usr/local/bin/block-ip.sh;
-  sudo echo $"  /sbin/ipset add honeypot4 \${REMOTE_ADDR}" >> /usr/local/bin/block-ip.sh;
-  sudo echo $"  /sbin/conntrack -D -s \${REMOTE_ADDR}" >> /usr/local/bin/block-ip.sh;
-  sudo echo #"  echo "deny ${REMOTE_ADDR};" >> /etc/nginx/conf.d/blacklist.conf;" >>/usr/local/bin/block-ip.sh;
-  sudo echo #"  nginx -s reload" >>/usr/local/bin/block-ip.sh;
-  sudo echo $"elif [[ \"\$REMOTE_ADDR\" != \"\${1#*:[0-9a-fA-F]}\" ]]; then" >> /usr/local/bin/block-ip.sh;
-  sudo echo $"  /sbin/ipset add honeypot6 \${REMOTE_ADDR}" >> /usr/local/bin/block-ip.sh;
-  sudo echo $"  /sbin/conntrack -D -s \${REMOTE_ADDR}" >> /usr/local/bin/block-ip.sh;
-  sudo echo #"  echo "deny ${REMOTE_ADDR};" >> /etc/nginx/conf.d/blacklist.conf;" >>/usr/local/bin/block-ip.sh;
-  sudo echo #"  nginx -s reload" >>/usr/local/bin/block-ip.sh;
-  sudo echo $"else" >> /usr/local/bin/block-ip.sh;
-  sudo echo $"  echo \"Unrecognized Ip format '\$1'\"" >> /usr/local/bin/block-ip.sh;
-  sudo echo $"fi" >> /usr/local/bin/block-ip.sh;
+  if [[ ! -f "/usr/local/bin/block-ip.sh" ]]; then
+    sudo touch /usr/local/bin/block-ip.sh;
+    sudo echo $"#!/bin/bash" >> /usr/local/bin/block-ip.sh;
+    sudo echo $"" >> /usr/local/bin/block-ip.sh;
+    sudo echo $"if [[ -z \${REMOTE_ADDR} ]]; then" >> /usr/local/bin/block-ip.sh;
+    sudo echo $"  if [[ -z \"\$1\"  ]]; then" >> /usr/local/bin/block-ip.sh;
+    sudo echo $"    echo \"REMOTE_ADDR not set!\"" >> /usr/local/bin/block-ip.sh;
+    sudo echo $"    exit 1" >> /usr/local/bin/block-ip.sh;
+    sudo echo $"  else" >> /usr/local/bin/block-ip.sh;
+    sudo echo $"    REMOTE_ADDR=\$1" >> /usr/local/bin/block-ip.sh;
+    sudo echo $"  fi" >> /usr/local/bin/block-ip.sh;
+    sudo echo $"fi" >> /usr/local/bin/block-ip.sh;
+    sudo echo $"" >> /usr/local/bin/block-ip.sh;
+    sudo echo $"# Put space separate list of trusted IP addresses, not to lock yourself out if you like to test the honeypot! \:\)" >> /usr/local/bin/block-ip.sh;
+    sudo echo $"TRUSTED_IPS=(127.0.0.1)" >> /usr/local/bin/block-ip.sh;
+    sudo echo $"if printf '%s\n' \${TRUSTED_IPS[@]} | grep -q -P \"^\$REMOTE_ADDR\\\$\"; then" >> /usr/local/bin/block-ip.sh;
+    sudo echo $"  echo \"Trusted IP\"" >> /usr/local/bin/block-ip.sh;
+    sudo echo $"  exit 0" >> /usr/local/bin/block-ip.sh;
+    sudo echo $"fi" >> /usr/local/bin/block-ip.sh;
+    sudo echo $"" >> /usr/local/bin/block-ip.sh;
+    sudo echo $"if [[ \"\$REMOTE_ADDR\" != \"\${1#*[0-9].[0-9]}\" ]]; then" >> /usr/local/bin/block-ip.sh;
+    sudo echo $"  /sbin/ipset add honeypot4 \${REMOTE_ADDR}" >> /usr/local/bin/block-ip.sh;
+    sudo echo $"  /sbin/conntrack -D -s \${REMOTE_ADDR}" >> /usr/local/bin/block-ip.sh;
+    sudo echo #"  echo "deny ${REMOTE_ADDR};" >> /etc/nginx/conf.d/blacklist.conf;" >>/usr/local/bin/block-ip.sh;
+    sudo echo #"  nginx -s reload" >>/usr/local/bin/block-ip.sh;
+    sudo echo $"elif [[ \"\$REMOTE_ADDR\" != \"\${1#*:[0-9a-fA-F]}\" ]]; then" >> /usr/local/bin/block-ip.sh;
+    sudo echo $"  /sbin/ipset add honeypot6 \${REMOTE_ADDR}" >> /usr/local/bin/block-ip.sh;
+    sudo echo $"  /sbin/conntrack -D -s \${REMOTE_ADDR}" >> /usr/local/bin/block-ip.sh;
+    sudo echo #"  echo "deny ${REMOTE_ADDR};" >> /etc/nginx/conf.d/blacklist.conf;" >>/usr/local/bin/block-ip.sh;
+    sudo echo #"  nginx -s reload" >>/usr/local/bin/block-ip.sh;
+    sudo echo $"else" >> /usr/local/bin/block-ip.sh;
+    sudo echo $"  echo \"Unrecognized Ip format '\$1'\"" >> /usr/local/bin/block-ip.sh;
+    sudo echo $"fi" >> /usr/local/bin/block-ip.sh;
+  fi
 
-  sudo touch /etc/nginx/conf.d/blacklist.conf
+  if [[ ! -f "/etc/nginx/conf.d/blacklist.conf" ]]; then
+    sudo touch /etc/nginx/conf.d/blacklist.conf
+  fi
 
   # nginx 권한
-  sudo echo $"Defaults!/usr/local/bin/block-ip.sh env_keep=REMOTE_ADDR" >> /etc/sudoers.d/nginx-block-ip;
-  sudo echo $"nginx ALL=(ALL) NOPASSWD: /usr/local/bin/block-ip.sh" >> /etc/sudoers.d/nginx-block-ip;
+  if [[ ! -f "/etc/sudoers.d/nginx-block-ip" ]]; then
+    sudo echo $"Defaults!/usr/local/bin/block-ip.sh env_keep=REMOTE_ADDR" >> /etc/sudoers.d/nginx-block-ip;
+    sudo echo $"nginx ALL=(ALL) NOPASSWD: /usr/local/bin/block-ip.sh" >> /etc/sudoers.d/nginx-block-ip;
+  fi
 
   sudo firewall-cmd --reload;
 
-  sudo echo {\"type\": \"service_account\", \"project_id\": \"cultivated-cove-320001\",  \"private_key_id\": \"$CAPTCHA_KEY_ID\",     \"private_key\": \"$CAPTCHA_PEM\", \"client_email\": \"$CAPTCHA_EMAIL\",  \"client_id\": \"115405318409515778219\",     \"auth_uri\": \"https://accounts.google.com/o/oauth2/auth\",  \"token_uri\": \"https://oauth2.googleapis.com/token\",     \"auth_provider_x509_cert_url\": \"https://www.googleapis.com/oauth2/v1/certs\", \"client_x509_cert_url\":\" $CAPTCHA_X509\" } >> /home/ec2-user/credentials.json
-
+  if [[ ! -f "/home/webapp/credentials.json" ]];
+    sudo echo {\"type\": \"service_account\", \"project_id\": \"cultivated-cove-320001\",  \"private_key_id\": \"$CAPTCHA_KEY_ID\",     \"private_key\": \"$CAPTCHA_PEM\", \"client_email\": \"$CAPTCHA_EMAIL\",  \"client_id\": \"115405318409515778219\",     \"auth_uri\": \"https://accounts.google.com/o/oauth2/auth\",  \"token_uri\": \"https://oauth2.googleapis.com/token\",     \"auth_provider_x509_cert_url\": \"https://www.googleapis.com/oauth2/v1/certs\", \"client_x509_cert_url\":\" $CAPTCHA_X509\" } >> /home/webapp/credentials.json
+  fi
+  
   # elastic
   curl -L -O https://artifacts.elastic.co/downloads/beats/elastic-agent/elastic-agent-7.15.2-amd64.deb;
   sudo dpkg -i elastic-agent-7.15.2-amd64.deb;
